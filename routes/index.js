b23717cde073e9af5c65ca5b799d598ef6fb67f8
Greg JEFTIC <ajkula@gmail.com>
2016-11-10 05:43:09 +0100

    movies displayed by actor/title/tagline/roles... working well!

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var neo4j = require('neo4j-driver').v1;
 
-var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "tedrzi"));
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "azerty"));
 var session = driver.session();
 
 
@@ -14,7 +14,9 @@ router.get('/', function (req, res, next) {
         actor = req.param("actor");
 
     }
-    session.run("MATCH (a:Person)-[b:ACTED_IN]->(c:Movie) RETURN a.name AS name, b AS roles, c AS movies")
+//    session.run("MATCH (a:Person)-[b:ACTED_IN]->(c:Movie) RETURN distinct a.name AS name, b AS roles, c AS movies")
+//    session.run("MATCH (a:Person)-[b:ACTED_IN]->(c:Movie) with a, b, c order by a.name RETURN distinct a.name AS name, collect(b) AS roles, collect(c) AS movies")
+    session.run("MATCH (a:Person)-[b:ACTED_IN]->(c:Movie) with a, b.roles as roles, c as movies order by a.name RETURN distinct a.name AS name, collect(roles + movies) as movies")
 
         .then(function (result) {
             response = {
@@ -24,8 +26,8 @@ router.get('/', function (req, res, next) {
                 index: actor
             };
             res.render('index', response);
-            console.log(response.list[0]._fields[2].properties.title);
-            console.log(response.list[0]._fields[1].properties.roles[0]);
+            console.log(response.list[0]._fields[0]);
+            console.log(response.list[0]._fields[1]);
             console.log(response.index)
         });
 
